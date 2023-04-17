1de8336a4c12f5ee2f70a9184021014daf10b1d9
Diego Danelone <ddanelone@frsf.utn.edu.ar>
2023-04-16 22:03:49 -0300

    Login y las primeras dos interfaces desarrolladas

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import views.LoginView;
+
+public class Main {
+    public static void main(String[] args) {
+        //Instancia del Login
+        LoginView login = new LoginView();
+        login.setVisible(true);
+    }
+}
